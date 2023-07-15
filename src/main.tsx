2ca52a5bd250df91128acc46820da445bc8920cd
Egor <belaz.egor@gmail.com>
2023-07-15 14:45:18 +0300

    fix: add pnpm

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import './styles/index.scss'
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
@@ -7,7 +6,7 @@ import { App } from './App.tsx'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>
