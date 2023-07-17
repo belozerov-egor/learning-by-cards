66360dcf66c6b44c579200ff5b30bb809870fd88
Egor <belaz.egor@gmail.com>
2023-07-17 23:13:18 +0300

    feat: add header

---
+++ src/components/ui/header/header.tsx
@@ -0,0 +1,15 @@
+import { Logo } from '../../../common/assets/img'
+import { Button } from '../button'
+
+export const Header = () => {
+  return (
+    <div>
+      <>
+        <Logo />
+      </>
+      <>
+        <Button variant={'primary'} />
+      </>
+    </div>
+  )
+}
