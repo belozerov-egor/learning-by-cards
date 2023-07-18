304e2ffb127d5eac4b860b2eac618706c7f3a3b8
Egor <belaz.egor@gmail.com>
2023-07-18 18:32:41 +0300

    fix: fix project

--- src/common/assets/img/index.ts
+++ src/common/assets/img/index.ts
@@ -1,1 +1,2 @@
 export * from './logo.tsx'
+export * from './logout.tsx'
--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,14 +1,15 @@
 import { Logo } from '../../../common/assets/img'
 import { Button } from '../button'
 
+import s from './header.module.scss'
 export const Header = () => {
   return (
-    <div>
-      <>
+    <div className={s.headerBlock}>
+      <div className={s.logoBlock}>
         <Logo />
-      </>
+      </div>
       <>
-        <Button variant={'primary'} />
+        <Button variant={'primary'}>Sign In</Button>
       </>
     </div>
   )
