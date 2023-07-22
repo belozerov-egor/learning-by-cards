03048eb1cfd201189fac7d8552a05eab46f23e5e
Egor <belaz.egor@gmail.com>
2023-07-22 11:31:43 +0300

    fix: fix drop-down

---
+++ src/components/ui/dropDownMenu/dropDownMenu.tsx
@@ -0,0 +1,40 @@
+import { FC, ReactNode } from 'react'
+
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+
+import './style.css'
+
+import { Avatar } from '../../../common/assets/img'
+
+type DropDownMenuPropsType = {
+  trigger?: ReactNode
+}
+
+export const DropDownMenuDemo: FC<DropDownMenuPropsType> = () => {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button className="IconButton" aria-label="Customise options">
+          <Avatar />
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
+          <DropdownMenu.Item className="DropdownMenuItem">
+            New Tab <div className="RightSlot">⌘+T</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Separator className="DropdownMenuSeparator" />s
+          <DropdownMenu.Item className="DropdownMenuItem">
+            New Window <div className="RightSlot">⌘+N</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Separator className="DropdownMenuSeparator" />
+          <DropdownMenu.Item className="DropdownMenuItem" disabled>
+            New Private Window <div className="RightSlot">⇧+⌘+N</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Arrow className="DropdownMenuArrow" />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+}
