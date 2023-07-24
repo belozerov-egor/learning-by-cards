2d86738ab09c5fc8c5ded2c651b7969dceb2b057
Egor <belaz.egor@gmail.com>
2023-07-24 23:25:22 +0300

    fix: fix drop-down

--- src/components/ui/dropDownMenu/dropDownMenu.tsx
+++ src/components/ui/dropDownMenu/dropDownMenu.tsx
@@ -2,7 +2,8 @@ import { FC, ReactNode } from 'react'
 
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
-import { Avatar } from '../../../common/assets/img'
+import { Avatar, Logout, Profile } from '../../../assets/icons'
+import { Typography } from '../typography'
 
 import s from './dropDownMenu.module.scss'
 
@@ -19,25 +20,36 @@ export const DropDownMenuDemo: FC<DropDownMenuPropsType> = () => {
             <Avatar />
           </button>
         </DropdownMenu.Trigger>
-
         <DropdownMenu.Portal>
           <DropdownMenu.Content className={s.dropdownMenuContent} sideOffset={5}>
             <DropdownMenu.Item className={s.dropdownMenuItem}>
-              <Avatar />{' '}
-              <div>
-                <span>name</span>
-                <span>email</span>
+              <div className={s.dropdownMenuItemInfo}>
+                <Avatar />
+                <div className={s.info}>
+                  <Typography variant={'subtitle2'}>Name</Typography>
+                  <Typography variant={'caption'} className={s.email}>
+                    egor.belozerov@mail.ru
+                  </Typography>
+                </div>
               </div>
             </DropdownMenu.Item>
             <DropdownMenu.Separator className={s.dropdownMenuSeparator} />
-            <DropdownMenu.Item className="DropdownMenuItem">
-              New Window <div className="RightSlot">⌘+N</div>
+            <DropdownMenu.Item className={s.dropdownMenuItem}>
+              <div className={s.dropdownMenuItemContent}>
+                <Profile />
+                <Typography variant={'caption'}>My Profile</Typography>
+              </div>
             </DropdownMenu.Item>
             <DropdownMenu.Separator className={s.dropdownMenuSeparator} />
-            <DropdownMenu.Item className="DropdownMenuItem" disabled>
-              New Private Window <div className="RightSlot">⇧+⌘+N</div>
+            <DropdownMenu.Item className={s.dropdownMenuItem}>
+              <div className={s.dropdownMenuItemContent}>
+                <Logout />
+                <Typography variant={'caption'}>Sign out</Typography>
+              </div>
             </DropdownMenu.Item>
-            <DropdownMenu.Arrow className={s.dropdownMenuArrow} />
+            <DropdownMenu.Arrow className={s.arrowBox} asChild>
+              <div className={s.arrow} />
+            </DropdownMenu.Arrow>
           </DropdownMenu.Content>
         </DropdownMenu.Portal>
       </div>
