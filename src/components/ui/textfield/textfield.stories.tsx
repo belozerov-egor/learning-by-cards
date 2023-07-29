28ea29e922792aba7af3b16547806d9ee3953c75
Egor <belaz.egor@gmail.com>
2023-07-29 17:35:43 +0300

    fix: fix input, add new name Textfield

--- src/components/ui/textfield/textfield.stories.tsx
+++ src/components/ui/textfield/textfield.stories.tsx
@@ -1,57 +1,65 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Input } from './input.tsx'
+import { TextField } from './textfield.tsx'
 
 const meta = {
-  title: 'Components/Input',
-  component: Input,
+  title: 'Components/TextField',
+  component: TextField,
   tags: ['autodocs'],
-} satisfies Meta<typeof Input>
+  argTypes: { onChange: { action: 'text changes' } },
+} satisfies Meta<typeof TextField>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const InputDefault: Story = {
+export const TextFieldDefault: Story = {
   args: {
+    label: 'label',
     placeholder: 'Default',
+    value: '',
     type: 'default',
     disableValue: false,
   },
 }
-export const InputDefaultError: Story = {
+export const TextFieldDefaultError: Story = {
   args: {
     placeholder: 'Default',
     type: 'default',
+    value: '',
     disableValue: false,
     errorMessage: 'Error!',
   },
 }
-export const InputPassword: Story = {
+export const TextFieldPassword: Story = {
   args: {
     placeholder: 'Password',
     type: 'password',
+    value: 'Password',
     disableValue: false,
   },
 }
-export const InputPasswordError: Story = {
+export const TextFieldPasswordError: Story = {
   args: {
     placeholder: 'Password',
     type: 'password',
+    value: 'Some Error',
     disableValue: false,
     errorMessage: 'Error!',
   },
 }
-export const InputSearch: Story = {
+export const TextFieldSearch: Story = {
   args: {
     placeholder: 'Search',
-    type: 'search',
+    type: 'searchType',
+    value: '',
     disableValue: false,
   },
 }
-export const InputSearchError: Story = {
+export const TextFieldSearchError: Story = {
   args: {
     placeholder: 'Search',
-    type: 'search',
+    type: 'searchType',
+    value: '',
     disableValue: false,
     errorMessage: 'Error!',
   },
---
+++ src/components/ui/textfield/textfield.tsx
@@ -0,0 +1,119 @@
+import { ChangeEvent, KeyboardEvent, ComponentPropsWithoutRef, forwardRef, useState } from 'react'
+
+import { DeleteIcon, Eye, NotEye, Search } from '../../../common/assets/img'
+import { LabelDemo } from '../label'
+import { Typography } from '../typography'
+
+import s from './textfield.module.scss'
+
+export type TextFieldProps = {
+  type: 'default' | 'password' | 'searchType'
+  label?: string
+  errorMessage?: string
+  placeholder?: string
+  disableValue?: boolean
+  value?: string
+  onChangeText?: (value: string) => void
+  onEnter?: () => void
+  onSearchClear?: () => void
+} & ComponentPropsWithoutRef<'input'>
+
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  ({
+    errorMessage,
+    label,
+    placeholder = 'Some text',
+    type = 'default',
+    disableValue = false,
+    value,
+    onEnter,
+    onSearchClear,
+    onChangeText,
+    ...restProps
+  }) => {
+    const [showPassword, setShowPassword] = useState(true)
+
+    const finalType = getType(type, showPassword)
+
+    const inputStyle = (type: 'default' | 'password' | 'searchType') => {
+      if (type === 'searchType') {
+        return { paddingLeft: '2.56rem', paddingRight: '35px' }
+      } else if (type === 'password') {
+        return { paddingRight: '35px' }
+      } else {
+        return {}
+      }
+    }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      onChangeText?.(e.currentTarget.value)
+    }
+
+    const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
+      onEnter && e.key === 'Enter' && onEnter()
+    }
+    const onSearchClearHandler = () => {
+      if (onSearchClear) {
+        onSearchClear()
+      }
+    }
+
+    return (
+      <LabelDemo label={label} variant={'secondary'}>
+        <div className={s.fieldContainer}>
+          {type === 'searchType' && (
+            <span className={s.search}>
+              <Search fill={disableValue ? '#4c4c4c' : '#808080'} />
+            </span>
+          )}
+          <input
+            className={`${s.field} ${errorMessage ? s.error : ''}`}
+            placeholder={placeholder}
+            type={finalType}
+            disabled={disableValue}
+            onChange={onChangeHandler}
+            onKeyPress={onKeyPressCallback}
+            style={inputStyle(type)}
+            value={value}
+            {...restProps}
+          />
+          {type === 'password' && (
+            <button
+              className={s.buttonAction}
+              type={'button'}
+              disabled={disableValue}
+              onClick={() => setShowPassword(prev => !prev)}
+            >
+              {showPassword ? (
+                <Eye fill={disableValue ? '#4c4c4c' : '#fff'} />
+              ) : (
+                <NotEye fill={disableValue ? '#4c4c4c' : '#fff'} />
+              )}
+            </button>
+          )}
+          {type === 'searchType' && (
+            <button
+              className={s.buttonAction}
+              type={'button'}
+              disabled={disableValue}
+              onClick={onSearchClearHandler}
+            >
+              <DeleteIcon fill={disableValue ? '#4c4c4c' : '#808080'} />
+            </button>
+          )}
+        </div>
+        <Typography variant="body1" className={s.errorMessage}>
+          {errorMessage}
+        </Typography>
+      </LabelDemo>
+    )
+  }
+)
+
+function getType(type: string, showPassword: boolean) {
+  if (type === 'password' && showPassword) {
+    return 'text'
+  }
+
+  return type
+}
