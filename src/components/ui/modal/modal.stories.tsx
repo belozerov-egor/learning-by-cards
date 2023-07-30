7a0d9513ec843d1f41f95c37750d9e20b5a6bc01
Egor <belaz.egor@gmail.com>
2023-07-30 20:52:14 +0300

    feat: add modal

---
+++ src/components/ui/modal/modal.stories.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+import type { Meta } from '@storybook/react'
+
+import { Button } from '../button'
+import { CheckBox } from '../checkbox'
+import { RadioGroupDemo } from '../radioGroup'
+import { TextField } from '../textfield'
+import { Typography } from '../typography'
+
+import { Modal } from './modal.tsx'
+
+const meta = {
+  title: 'Components/Modal',
+  component: Modal,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Modal>
+
+export default meta
+
+export const ModalDemo = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <Button variant={'primary'} onClick={handleOpen}>
+        Open Modal
+      </Button>
+      <Modal title={'hello'} showCloseButton={true} open={open} onClose={handleClose}>
+        <TextField type={'searchType'} />
+        <TextField type={'default'} />
+        <CheckBox variant={'default'} />
+        <Typography variant={'body1'}> Hello There!</Typography>
+        <RadioGroupDemo />
+      </Modal>
+    </>
+  )
+}
---
+++ src/components/ui/modal/modal.tsx
@@ -0,0 +1,58 @@
+import { ComponentProps, FC } from 'react'
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+} from '@radix-ui/react-dialog'
+
+import { Close } from '../../../common/assets/img'
+import { Typography } from '../typography'
+
+import s from './modal.module.scss'
+
+type PropsType = {
+  open: boolean
+  onClose?: () => void
+  showCloseButton?: boolean
+  title?: string
+} & ComponentProps<'div'>
+
+export const Modal: FC<PropsType> = ({
+  open = false,
+  title,
+  onClose,
+  children,
+  showCloseButton = true,
+}) => {
+  function handleModalClosed() {
+    onClose?.()
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={handleModalClosed}>
+      {open && (
+        <DialogPortal>
+          <DialogOverlay className={s.overlay} />
+          <DialogContent className={s.content}>
+            <header className={s.header}>
+              <DialogTitle asChild>
+                <Typography variant={'h2'}>{title}</Typography>
+              </DialogTitle>
+
+              {showCloseButton && (
+                <DialogClose className={s.closeButton}>
+                  <Close />
+                </DialogClose>
+              )}
+            </header>
+            <div className={s.contentBox}>{children}</div>
+          </DialogContent>
+        </DialogPortal>
+      )}
+    </Dialog>
+  )
+}
