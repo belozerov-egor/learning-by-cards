3536bfd52cc6c74075ea93fc3b741a2f8452ce0f
Egor <belaz.egor@gmail.com>
2023-07-31 17:49:13 +0300

    fix: fix personal-information

--- src/components/auth/personal-information/personal-information.tsx
+++ src/components/auth/personal-information/personal-information.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
+
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
-import { AvatarInCard } from '../../../common'
+import { AvatarInCard, Edit, Logout } from '../../../common'
 import { Button, Card, ControlledTextField, Typography } from '../../ui'
 
 import s from './personalInformation.module.scss'
@@ -13,6 +15,7 @@ const sigInSchema = z.object({
 
 type SignInFormShem = z.infer<typeof sigInSchema>
 export const PersonalInformation = () => {
+  const [editMode, setEditMode] = useState<boolean>(false)
   const { control, handleSubmit } = useForm<SignInFormShem>({
     resolver: zodResolver(sigInSchema),
   })
@@ -26,21 +29,51 @@ export const PersonalInformation = () => {
         Personal Information
       </Typography>
       <div className={s.avatarBlock}>
-        <AvatarInCard />
+        <div className={s.avatar}>
+          <AvatarInCard />
+          {!editMode && (
+            <div className={s.avatarEdit}>
+              <Edit />
+            </div>
+          )}
+        </div>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <ControlledTextField
-          name={'nickName'}
-          label={'Nickname'}
-          type={'default'}
-          control={control}
-          className={s.nickName}
-          placeholder={'Nickname'}
-        />
-        <Button fullWidth={true} className={s.submit} type="submit">
-          Save Changes
-        </Button>
-      </form>
+      {editMode ? (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <ControlledTextField
+            name={'nickName'}
+            label={'Nickname'}
+            type={'default'}
+            control={control}
+            className={s.editNickName}
+            placeholder={'Nickname'}
+          />
+          <Button
+            fullWidth={true}
+            className={s.submit}
+            type="submit"
+            onClick={() => setEditMode(false)}
+          >
+            Save Changes
+          </Button>
+        </form>
+      ) : (
+        <div className={s.infoBlock}>
+          <div className={s.nameBlock}>
+            <Typography variant={'h1'} className={s.name}>
+              Ivan
+            </Typography>
+            <Edit onClick={() => setEditMode(true)} />
+          </div>
+          <Typography variant={'body2'} as={'span'} className={s.email}>
+            j&johnson@gmail.com
+          </Typography>
+          <Button variant={'secondary'} className={s.logout}>
+            <Logout />
+            <Typography variant={'subtitle2'}>Logout</Typography>
+          </Button>
+        </div>
+      )}
     </Card>
   )
 }
