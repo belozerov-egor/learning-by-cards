13b00d428358c96da37d8d3f1640646d02950150
Egor <belaz.egor@gmail.com>
2023-08-02 23:59:51 +0300

    fix: fix routing

--- src/common/assets/img/close.tsx
+++ src/common/assets/img/close.tsx
@@ -10,7 +10,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
     ref={ref}
     {...props}
   >
-    <g clipPath="url(#prefix__clip0_124_21523)">
+    <g>
       <path
         d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"
         fill="currentColor"
--- src/components/auth/sign-in/sign-in.tsx
+++ src/components/auth/sign-in/sign-in.tsx
@@ -1,5 +1,7 @@
+import { DevTool } from '@hookform/devtools'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { z } from 'zod'
 
 import { Button, Card, ControlledCheckbox, ControlledTextField, Typography } from '../../ui'
@@ -27,10 +29,12 @@ export const SignIn = () => {
         Sign In
       </Typography>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <DevTool control={control} />
         <ControlledTextField
           name={'email'}
           label={'Email'}
           type={'default'}
+          placeholder={'enter your email'}
           control={control}
           className={s.email}
         />
@@ -38,6 +42,7 @@ export const SignIn = () => {
           name={'password'}
           label={'Password'}
           type={'password'}
+          placeholder={'enter your password'}
           control={control}
           className={s.password}
         />
@@ -48,7 +53,7 @@ export const SignIn = () => {
           checkBoxText={'Remember me'}
         />
         <div className={s.forgotWrapper}>
-          <Button as={'a'} variant={'link'}>
+          <Button as={Link} to="/forgot-password" variant={'link'} className={s.forgotPassword}>
             <Typography variant={'body2'}>Forgot Password?</Typography>
           </Button>
         </div>
@@ -59,7 +64,7 @@ export const SignIn = () => {
       <Typography variant={'body2'} className={s.question}>
         Don&apos;t have an account?
       </Typography>
-      <Button as={'a'} variant={'link'} className={s.signUp}>
+      <Button as={Link} to="/sign-up" variant={'link'} className={s.signUp}>
         Sign Up
       </Button>
     </Card>
