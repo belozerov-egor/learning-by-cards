58c1008d87854ef9f1a87475b8a43264a1df4e53
Egor <belaz.egor@gmail.com>
2023-08-08 15:59:51 +0300

    feat: add empty pack

---
+++ src/page/empty-pack/empty-pack.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react'
+
+import { Link } from 'react-router-dom'
+
+import { Back } from '../../common'
+import { Button, Typography } from '../../components'
+
+import s from './empty-pack.module.scss'
+
+type PropsType = {
+  name?: string
+}
+export const EmptyPack: FC<PropsType> = ({ name }) => {
+  return (
+    <div className={s.emptyPackBlock}>
+      <Button as={Link} to="/" variant={'link'} className={s.backButton}>
+        <Back />
+        Back to Packs List
+      </Button>
+      <Typography variant={'large'} className={s.title}>
+        {name}
+      </Typography>
+      <Typography variant={'body1'} className={s.description}>
+        This pack is empty. Click add new card to fill this pack
+      </Typography>
+      <div className={s.addNewPackButton}>
+        <Button variant={'primary'}>Add New Card</Button>
+      </div>
+    </div>
+  )
+}
